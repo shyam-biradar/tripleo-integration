dca3302a6347abcea818ed66cbae51333a8a926d
shyam-biradar <shyam.biradar@triliodata.com>
2017-12-15 14:33:07 +0530

    Moved class arguments to class variables section

--- puppet/trilio/manifests/init.pp
+++ puppet/trilio/manifests/init.pp
@@ -1,22 +1,25 @@
 class trilio (
-  $contego_user				= 'nova',
-  $contego_group			= 'nova',
-  $contego_conf_file			= "/etc/tvault-contego/tvault-contego.conf",
-  $contego_groups			= ['kvm','qemu','disk'],
-  $vault_data_dir			= '/var/triliovault-mounts',
-  $vault_data_dir_old			= '/var/triliovault',
-  $contego_dir				= '/home/tvault',
-  $contego_virtenv_dir			= '${contego_dir}/.virtenv',
-  $log_dir				= '/var/log/nova',
-  $contego_bin				= '${contego_virtenv_dir}/bin/tvault-contego',
-  $contego_python			= '${contego_virtenv_dir}/bin/python',
-  $config_files				= '--config-file=${nova_dist_conf_file} --config-file=${nova_conf_file} --config-file=${contego_conf_file}',
   $nova_conf_file			= '/etc/nova/nova.conf',
   $nova_dist_conf_file			= '/usr/share/nova/nova-dist.conf',
   $nova_compute_filters_file		= '/usr/share/nova/rootwrap/compute.filters',
   $nfs_shares				= '192.168.1.33:/mnt/tvault',
   $nfs_options				= '',
   $tvault_appliance_ip			= '192.168.1.122',
+) {
+
+  $contego_user				= 'nova'
+  $contego_group			= 'nova'
+  $contego_conf_file			= "/etc/tvault-contego/tvault-contego.conf"
+  $contego_groups			= ['kvm','qemu','disk']
+  $vault_data_dir			= "/var/triliovault-mounts"
+  $vault_data_dir_old			= "/var/triliovault"
+  $contego_dir				= "/home/tvault"
+  $contego_virtenv_dir			= "${contego_dir}/.virtenv"
+  $log_dir				= "/var/log/nova"
+  $contego_bin				= "${contego_virtenv_dir}/bin/tvault-contego"
+  $contego_python			= "${contego_virtenv_dir}/bin/python"
+  $config_files				= "--config-file=${nova_dist_conf_file} --config-file=${nova_conf_file} --config-file=${contego_conf_file}"
+  
   $log_rotate_file_content		= "/var/log/nova/tvault-contego.log {
 daily
 missingok
@@ -25,7 +28,8 @@ copytruncate
 size=25M
 rotate 3
 compress
-}",
+}
+
   $contengo_systemd_file_content	= "[Unit]
 Description=Tvault contego
 After=openstack-nova-compute.service
@@ -33,26 +37,28 @@ After=openstack-nova-compute.service
 User=nova
 Group=nova
 Type=simple
-ExecStart=/home/tvault/.virtenv/bin/python /home/tvault/.virtenv/bin/tvault-contego --config-file=/usr/share/nova/nova-dist.conf --config-file=/etc/nova/nova.conf --config-file=/etc/tvault-contego/tvault-contego.conf
+ExecStart=${contego_python} ${contego_bin} ${config_files}
 TimeoutStopSec=20
 KillMode=process
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target
-",
+"
+
   $contego_conf_file_content		= "[DEFAULT]
-vault_storage_nfs_export = 192.168.1.33:/mnt/tvault
-vault_storage_nfs_options = 
+vault_storage_nfs_export = ${nfs_shares}
+vault_storage_nfs_options = ${nfs_options}
 vault_storage_type = nfs
-vault_data_directory_old = /var/triliovault
-vault_data_directory = /var/triliovault-mounts
+vault_data_directory_old = ${vault_data_dir_old}
+vault_data_directory = ${vault_data_dir}
 log_file = /var/log/nova/tvault-contego.log
 debug = False
 verbose = True
 max_uploads_pending = 3
 max_commit_pending = 3
-",
-) {
+"
+
+
 ## Adding passwordless sudo access to 'nova' user
     file { "/etc/sudoers.d/${contego_user}":
       ensure => present,
