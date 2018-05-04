ae1af5c240a60bf9c0d70d76bc988fe0ce44b7e3
shyam-biradar <shyam.biradar@trilio.io>
2018-05-04 02:24:08 -0400

    Changing default values os some class parameters to undef

--- puppet/trilio/manifests/extension.pp
+++ puppet/trilio/manifests/extension.pp
@@ -1,10 +1,9 @@
 class trilio::extension (
   $nova_conf_file			= '/etc/nova/nova.conf',
   $nova_dist_conf_file			= '/usr/share/nova/nova-dist.conf',
-  $nova_compute_filters_file		= '/usr/share/nova/rootwrap/compute.filters',
-  $nfs_shares				= '192.168.2.35:/mnt/tvault',
-  $nfs_options				= '',
-  $tvault_appliance_ip			= '192.168.2.125',
+  $nfs_shares				= undef,
+  $nfs_options				= undef,
+  $tvault_appliance_ip			= undef,
 ) {
 
   $contego_user				= 'nova'
